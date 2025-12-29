c73382cd679d7ab9fcc3b21829c6eb7e21241be1
Amie-dev <learn.amie01@gmail.com>
2025-12-29 18:17:25 +0530

    04-cards-project completed with passing props

--- 04-cards-project/src/components/Card.jsx
+++ 04-cards-project/src/components/Card.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
 
-function Card() {
+function Card(props) {
   return (
     <div className="card">
         <div className="part1">
           <div className="header">
-            <img src="" alt="" />
+            <img src={props.brandLogo} alt="" />
             <button>save</button>
           </div>
           <div className="body">
             <div className="companyName">
-              <h3>Amazon</h3>
-              <p>6day ago</p>
+              <h3>{props.companyName}</h3>
+              <p>{props.datePosted}</p>
             </div>
             <div className="role">
-              <h4>Senior UI/UX Designer</h4>
+              <h4>{props.post}</h4>
             </div>
             <div className="types">
-              <button className="btn1">Part Time</button>
-              <button className="btn2">Senior Level</button>
+              <button className="btn1">{props.tag1}</button>
+              <button className="btn2">{props.tag2}</button>
             </div>
           </div>
         </div>
         <div className="part2">
           <div className="hour">
-            <h4>$120/hr</h4>
-            <p>Mumbai, India</p>
+            <h4>{props.pay}</h4>
+            <p>{props.location}</p>
           </div>
           <button className="apply">
             Apply Now
